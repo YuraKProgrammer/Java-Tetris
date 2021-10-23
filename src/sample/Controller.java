8a2ede94e69d14b9ba325a85d78435681615b499
YuraKProgrammer <yura.kalantyr@mail.ru>
2021-10-23 10:54:04 +0300

    Стартовое окно

---
+++ src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.scene.Scene;
+
+public class Controller {
+    private Scene scene;
+    public void setScene(Scene scene) {
+        this.scene = scene;
+/*
+        _start.setOnAction(actionEvent -> {
+            try {
+                showGameWindow();
+            }
+            catch (Exception e){
+                Main.showError(e);
+            }
+        });
+ */
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        var loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("StartWindow.fxml")));
+        var root = (Parent)loader.load();
+        primaryStage.setTitle("Tetris");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        var controller = loader.<Controller>getController();
+        controller.setScene(primaryStage.getScene());
+    }
+
+    public static void showError(Exception exc) {
+        var alert = new Alert(Alert.AlertType.ERROR, exc.getMessage());
+        alert.showAndWait();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
